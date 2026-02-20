3dd6d745db352bfd057486ae6c62fd8bc0d9bda2
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-20 15:38:20 +0100

    feat: aggiungo ora la lista statica di prodotti in ProductsPage

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -4,6 +4,13 @@ export default function ProductsPage() {
 
   const { budgetMode } = useBudget();
 
+  const products = [
+    { id: 1, title: "Prodotto A", price: 20 },
+    { id: 2, title: "Prodotto B", price: 50 },
+    { id: 3, title: "Prodotto C", price: 15 },
+  ];
+  
+
   
 
   return (
