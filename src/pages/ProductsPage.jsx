57279252fbd851b847a73d6e33d6b1b3828c2b38
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-20 15:25:14 +0100

    fix: cancello import axios e useEffect da ProductsPage perché non necessari

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,10 +1,10 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { useBudget } from "../contexts/BudgetContext";
 
 export default function ProductsPage() {
-  const [products, setProducts] = useState([]);
+
   const { budgetMode } = useBudget();
+
+  
 
   return (
      
